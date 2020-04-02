e2e4f54e6947aa5f89563219d37993252ac624f5
DatNguyen98 <monnguyen789@gmail.com>
2020-04-02 22:25:43 +0700

    UI - Store

--- KLTN.Web/KLTN.Web/ClientApp/src/app/pages/page-client/store/store.component.ts
+++ KLTN.Web/KLTN.Web/ClientApp/src/app/pages/page-client/store/store.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
+  products: any[] = [
+    {
+      code: 1,
+      name: "Gấp đôi hoặc mất hết",
+      descripte: "Streak Freeze cho phép bạn giữ nguyên streak trong một ngày bạn không có hoạt động nào",
+      image: "/assets/images/product-01.png",
+      value: 10
+    },
+    {
+      code: 2,
+      name: "Luyện tập có tính thời gian",
+      descripte: "Hãy xem bạn tập luyện các kỹ năng tốt tới đâu với đồng hồ trong Thử thách bấm giờ.",
+      image: "/assets/images/product-02.png",
+      value: 10
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+ 
